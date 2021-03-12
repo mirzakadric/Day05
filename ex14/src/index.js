68f9986b7517883e3b03f917768f69d1774628e5
unknown <kadric.mirza@hotmail.com>
2021-03-12 15:42:09 +0100

    Adding exercises for Day05

---
+++ ex14/src/index.js
@@ -0,0 +1,18 @@
+function main(){
+    // Only change code below this line
+
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+
+    var wordBlanks = "My beautiful " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + ".";
+
+    //Only change code above this line
+
+    return wordBlanks;
+}
+
+console.log(main());
+
+module.exports = main;
